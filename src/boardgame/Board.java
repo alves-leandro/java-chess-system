780cea98dbdbf087728cfb0bf1eca0988113e900
alves-leandro <l.alves85@live.com>
2024-04-01 17:20:37 -0300

    Feat: Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,6 +38,12 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
+	// Criar Method: Board.PlacePiece(piece, position)
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 	
 	
 }
